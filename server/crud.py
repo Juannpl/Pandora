897925840d124562f90358683d04cd936e6aba94
Juannpl <london_j@etna-alternance.net>
2024-09-23 15:47:05 +0200

    git clone

--- server/crud.py
+++ server/crud.py
@@ -3,11 +3,17 @@ from .models import User
 from .schemas import UserCreate
 
 def create_user(db: Session, user: UserCreate):
+    # Check if the user already exists
+    existing_user = db.query(User).filter(User.email == user.email).first()
+    if existing_user:
+        raise ValueError("Email already registered.")
+    
     db_user = User(name=user.name, email=user.email, age=user.age)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
     return db_user
+
 
 def get_users(db: Session, skip: int = 0, limit: int = 10):
     return db.query(User).offset(skip).limit(limit).all()
--- server/database.py
+++ server/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://myuser:mypassword@localhost:5432/mydatabase"
+DATABASE_URL = "postgresql://user:root@localhost:5432/pandora_db"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
